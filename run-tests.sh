fc8413d1a01632b418f74d3105881ece63e7681f
lenarother <rother.magdalena@gmail.com>
2014-09-11 13:10:40 +0200

    run-tests.sh modified to add moderna and moderna/tests/test_data to PYTHONPATH.

--- run-tests.sh
+++ run-tests.sh
@@ -1,2 +1,10 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+#echo $DIR
+MYVALUE="$DIR/tests/test_data"
+#echo "$MYVALUE"
+export PYTHONPATH=${PYTHONPATH}:$DIR:$MYVALUE
+#echo ${PYTHONPATH}
 cd tests
 python test_ModeRNA_all.py
